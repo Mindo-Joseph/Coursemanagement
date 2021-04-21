716dc89ad2f1215873422706fdc8ba97737309ab
Mindo-Joseph <mindo.joseph.mj@gmail.com>
2021-04-21 16:19:56 +0300

    Add middleware to store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
+import reduxImmutabelStateInvariant from 'redux-immutable-state-invariant';
 
 
 export default function configureStore(initialState) {
-    return createStore(rootReducer, initialState);
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+    return createStore(
+        rootReducer,
+        initialState,
+        composeEnhancers(applyMiddleware(reduxImmutabelStateInvariant())),
+
+    );
 }
