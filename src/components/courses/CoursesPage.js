1f68d0c244975bb30037f3745d313762e1a0b0e0
Mindo-Joseph <mindo.joseph.mj@gmail.com>
2021-04-21 16:11:45 +0300

    Move courses page to components folder

---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+class CoursesPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            course: {
+                title: ''
+            }
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event) {
+        const course = { ...this.state.course, title: event.target.value }
+        this.setState({course})
+    
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        alert(this.state.course.title)
+    }
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <h1>CoursesPage</h1>
+                <h3>Add course</h3>
+                <input type="text" onChange={this.handleChange} value={this.state.course.title} />
+                <input type="submit" value="Save"/>
+            </form>
+        )
+    }
+}
+export default CoursesPage;
